7e997253f84a3583719e4be088cd616536c2b0f2
Mauricio <wmedinap@ulasalle.edu.pe>
2019-05-10 21:27:57 -0500

    builder

--- Builder/Builder.cpp
+++ Builder/Builder.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 class VRam{
     public:
-        int c;
+        string c;
 };
 
 class Procesador{
@@ -15,7 +15,7 @@ class Procesador{
 
 class Ram{
     public:
-        int c;
+        string c;
 };
 
 class PC{
@@ -23,99 +23,161 @@ public:
     Procesador* pro;
     VRam*  vr;
     Ram* rm;
+
+    PC(){
+        rm = new Ram();
+        rm->c = "vacio";
+        pro = new Procesador();
+        pro->nom ="vacio";
+        vr = new VRam();
+        vr->c = "vacio";
+            
+    }
     
-    void caracteristicas(){
+    void Caracteristicas(){
         cout << "Procesador:" << pro->nom << endl;
         cout << "VRam:" << vr->c << endl;
         cout << "Ram:" << rm->c << endl;
     }
 };
 
-class builder{
+class Builder{
 public:
     virtual Procesador* get_Procesador() = 0;
     virtual VRam* get_VRam() = 0;
     virtual Ram* get_Ram() = 0;
 };
 
-class director{
+class Director{
 private:
-    builder* buil;
+    Builder* buil;
 public:
-    void setbuilder(builder* b){
+    void setBuilder(Builder* b){
         buil = b;
     }
 
+    PC* set_PC(string A[], int n){
+            
+            
+            PC* pc = new PC();
+            for(int i = 0; i < n; i++){            
+                if(A[i] == "core i5" or A[i] == "amd ryzen") pc->pro->nom = A[i];
+                if(A[i] == "8gb ram" or A[i] == "16gb ram") pc->rm->c = A[i];
+                if(A[i] == "4gb vram" or A[i] == "12gb ram") pc->vr->c = A[i];
+            }
+            return pc;
+        }
+
     PC* get_PC(){
-        PC* comp = new PC();
-        comp->pro = buil->get_Procesador();
-        comp->vr = buil->get_VRam();
-        comp->rm = buil->get_Ram();
-        return comp;
+        PC* pc = new PC();
+        pc->pro = buil->get_Procesador();
+        pc->vr = buil->get_VRam();
+        pc->rm = buil->get_Ram();
+        return pc;
     }
 };
 
 
-class expensive : public builder{
+class Standard : public Builder{
+private:
+    Procesador* pro;
+    VRam*  vr;
+    Ram* rm;
 public:
-    VRam* get_VRam(){
-        VRam* v = new VRam();
-        v->c = 6;
-        return v;
+    Standard(){
+        pro = new Procesador();
+        pro->nom ="core i5";
+        vr = new VRam();
+        vr->c = "4bg vram";
+        rm = new Ram();
+        rm->c = "4gb ram";
     }
-
     Procesador* get_Procesador(){
-        Procesador* pro = new Procesador();
-        pro->nom = "Core i9";
         return pro;
     }
-
+    VRam* get_VRam(){
+        return vr;
+    }
     Ram* get_Ram(){
-        Ram* r = new Ram();
-        r->c = 16;
-        return r;
+        return rm;
+    }
+    void set_Procesador(string pro1){
+        pro->nom = pro1;
+    }
+    void set_vram(string c1){
+        vr->c = c1;
+    }
+    void set_ram(string c1){
+        rm->c = c1;
     }
 };
 
 
-class standard : public builder{
+class Expensive : public Builder{
+private:
+    Procesador* pro;
+    VRam*  vr;
+    Ram* rm;
 public:
-    VRam* get_VRam(){
-        VRam* v = new VRam();
-        v->c = 2;
-        return v;
+    Expensive(){
+        pro = new Procesador();
+        pro->nom ="amd ryzen";
+        vr = new VRam();
+        vr->c = "16gb ram";
+        rm = new Ram();
+        rm->c = "12gb ram";
     }
-
     Procesador* get_Procesador(){
-        Procesador* pro = new Procesador();
-        pro->nom = "core i5 8va generacion";
         return pro;
     }
-
+    VRam* get_VRam(){
+        return vr;
+    }
     Ram* get_Ram(){
-        Ram* r = new Ram();
-        r->c = 8;
-        return r;
+        return rm;
+    }
+    void set_Procesador(string pro1){
+        pro->nom = pro1;
+    }
+    void set_vram(string c1){
+        vr->c = c1;
+    }
+    void set_ram(string c1){
+        rm->c = c1;
     }
 };
 
 
 int main(){
-    PC* comp; 
-    director dire;
-    expensive MSI;
-    standard Lenovo;
-    cout << "MSI" << endl;
+    string arr[]={"AMD Ryzen", "intel inside", "4gb vram", "8gb ram"};
 
-    dire.setbuilder(&MSI); 
-    comp = dire.get_PC();
-    comp->caracteristicas();
-    cout << endl;
+    PC* pc; 
+    Director dire;
+
+    Standard Lenovo;
+    Expensive MSI;
+
+    Lenovo.set_Procesador("intel i3");
+    Lenovo.set_ram("6");
+    Lenovo.set_vram("2");
+
+    MSI.set_Procesador("intel i9");
+    MSI.set_vram("16");
+    MSI.set_ram("64");
+
     cout << "Lenovo" << endl;
+    dire.setBuilder(&Lenovo); 
+    pc = dire.get_PC();
+    pc->Caracteristicas();
+    cout << endl;
+    cout << "MSI" << endl;
+    dire.setBuilder(&MSI);
+    pc = dire.get_PC();
+    pc->Caracteristicas();
 
-    dire.setbuilder(&Lenovo);
-    comp = dire.get_PC();
-    comp->caracteristicas();
+    cout<<endl<<endl;
+    pc=dire.set_PC(arr,4);
+    pc->Caracteristicas();
 
     return 0;
 }
